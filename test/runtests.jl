9cdb097b2a0bf5e41b30e11389faa5a12d1f9284
willGraham01 <ccaegra@ucl.ac.uk>
2026-02-03 12:01:31 +0000

    Triple 2 is 6, Will

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Test
 @testset "MyJuliaPackage.jl" begin
     # Write your tests here.
     @test MyJuliaPackage.double(2) == 4
-    @test MyJuliaPackage.triple(2) == 8
+    @test MyJuliaPackage.triple(2) == 6
 end
