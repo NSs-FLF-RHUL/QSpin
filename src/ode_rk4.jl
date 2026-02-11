9e3a12b83bc0265e2ad7e3cf26bd7912521c1a30
Gary Liu <liueruey@hotmail.com>
2026-02-11 21:55:29 +0000

    Updating License, Project and Creating the QSpin module

--- src/ode_rk4.jl
+++ src/ode_rk4.jl
@@ -1,4 +1,12 @@
-function ode_rk4(u::Array{Float64},dt::Float64,time::Float64)
+"""
+Integrating an equation of motion usin the Runge-Kutta 4-th order method
+
+:param u: the targed solution of the equation of motion.
+:param δt: the Integrating time step.
+:param time: time.
+:param eom: the equation of motion of the problem.
+"""
+function ode_rk4(u::Array{Float64},dt::Float64,time::Float64,eom::function)   
     k1 = eom(u,time);
     k2 = eom(u+0.5*k1*dt,time+0.5*dt); 
     k3 = eom(u+0.5*k2*dt,time+0.5*dt); 
