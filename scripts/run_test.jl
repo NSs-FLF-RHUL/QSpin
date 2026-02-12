73f6f2b548af94159a84a44a5ef4848dffe0ad0a
Gary Liu <liueruey@hotmail.com>
2026-02-12 13:15:20 +0000

    Merge dev

--- scripts/run_test.jl
+++ scripts/run_test.jl
@@ -28,5 +28,5 @@ end
 
 # Running and Plotting
 @time ut, t = QSpin.OdeSolve.evolve_rk4([.1;.2],1e-3,1e-1,1.,eom)
-plot(t,ut[:,1])
-plot!(t,ut[:,2],xlabel = "time (A.U.)",ylabel="Rotating Frequency (A.U.)",title="Solving a set of coupled ODE")
+plot(t,ut[1,:])
+plot!(t,ut[2,:],xlabel = "time (A.U.)",ylabel="Rotating Frequency (A.U.)",title="Solving a set of coupled ODE")
