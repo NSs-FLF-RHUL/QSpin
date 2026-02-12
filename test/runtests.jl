0a4f44eb90a03084c936b69dc2ca68de4a5a5378
Gary Liu <liueruey@hotmail.com>
2026-02-12 15:22:23 +0000

    Update the runtest.jl for #6

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,27 @@
 using QSpin
 using Test
 
-@testset "QSpin.jl" begin
-    # Write your tests here.
-    @test QSpin.double(2) == 4
-    @test QSpin.triple(2) == 6
+@testset "ode_rk4" begin
+    # This begins a set of tests for the ode_rk4 function
+
+    @testset "ode_rk4 No Evolution" begin
+        # This begins a subset of tests for the ode_rk4 function,
+        # where we will use a time-independent field to confirm some simple properties.
+
+        # A simple "EoM" that doesn't evolve the field (dψdt = 0)
+        no_evolution(ψ, time) = zeros(size(ψ))
+        # An arbitrary initial field, that we don't expect to evolve
+        initial_field = [1.; 2.]
+
+        # Confirm that there is no evolution according to RK4
+        @test isapprox(
+            QSpin.OdeSolve.ode_rk4(initial_field, 1., 0., no_evolution),
+            initial_field
+        )
+        # When there is no evolution, the timestep δt should not matter
+        @test isapprox(
+            QSpin.OdeSolve.ode_rk4(initial_field, 1., 0., no_evolution),
+            QSpin.OdeSolve.ode_rk4(initial_field, 10., 0., no_evolution)
+        )
+    end
 end
