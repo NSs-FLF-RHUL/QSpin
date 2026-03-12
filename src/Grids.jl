7a472f10e314b56b0d133c6161070c3ac065e161
Gary Liu <liueruey@hotmail.com>
2026-03-12 10:19:28 +0000

    Unit test for Grids.CartGrid

--- src/Grids.jl
+++ src/Grids.jl
@@ -14,41 +14,49 @@ using ParallelStencil
 """
 function CartGrid(CompDomain::Array{Float64},GridSize::Array{Int64})
     dims = length(GridSize)
-    if dims <=2
+    if dims <=3
         Nx = GridSize[1]
         dx   = 2 * CompDomain[1]/ Nx
         x    = range(-CompDomain[1],stop=CompDomain[1]-dx,length=Int(Nx))
         kx   = [range(0,stop=Nx/2-1,length=Int(Nx/2)) ;range(-Nx/2,stop=-1,length=Int(Nx/2))]
         facx = pi / CompDomain[1]
+        kx  = kx .* facx
     end
-    if dims <=3
+    if dims >= 2
         Ny = GridSize[2]
         dy   = 2 * CompDomain[2] / Ny
         y    = range(-CompDomain[2],stop=CompDomain[2]-dy,length=Int(Ny))
         ky   = [range(0,stop=Ny/2-1,length=Int(Ny/2)) ;range(-Ny/2,stop=-1,length=Int(Ny/2))]
-        facy = pi / CompDomain[2]
-
-        X   = repeat(x',Ny,1)
-        Y   = repeat(y,1,Nx)
-        Kx  = repeat(kx',Ny,1)
-        Ky  = repeat(ky,1,Nx);
+        facy = pi / CompDomain[2] 
+        ky  = ky .* facy
     end
     if dims == 3 # In progress, not tested yet
         Nz = GridSize[3]
         dz   = 2 * CompDomain[3] / Nz
-        z    = range(-CompDomain[2],stop=CompDomain[2]-dz,length=Int(Nz))
+        z    = range(-CompDomain[3],stop=CompDomain[3]-dz,length=Int(Nz))
         kz   = [range(0,stop=Nz/2-1,length=Int(Nz/2)) ;range(-Nz/2,stop=-1,length=Int(Nz/2))]
-        facz = pi / CompDomain[2]
+        facz = pi / CompDomain[3]
+        kz   = kz .* facz
     end 
     if dims == 1
-        println("Creating 1D Cartesian grid with ", Nx, " points.")
+        println("    Creating 1D Cartesian grid with ", Nx, " points.")
         return x, kx, facx
     elseif dims == 2
-        println("Creating 2D Cartesian grid with ", Nx, " x ", Ny, " points.")
+        X   = repeat(x',Ny,1)
+        Y   = repeat(y,1,Nx)
+        Kx  = repeat(kx',Ny,1);
+        Ky  = repeat(ky,1,Nx);
+        println("    Creating 2D Cartesian grid with ", Nx, " x ", Ny, " points.")
         return x, y, X, Y, kx, ky,Kx, Ky, facx, facy
     elseif dims == 3
-        println("Creating 3D Cartesian grid with ", Nx, " x ", Ny, " x ", Nz, " points.")
-        return x, y, z, X, Y, Z, Kx, Ky, Kz, facx, facy, facz
+        X   = repeat(x',GridSize[2],1,GridSize[3]);
+        Y   = repeat(y,1,GridSize[1],GridSize[3]);
+        Z   = permutedims(repeat(z,1,GridSize[1],GridSize[2]),[2 3 1]); 
+        Kx  = repeat((kx)',GridSize[2],1,GridSize[3])
+        Ky  = repeat((ky),1,GridSize[1],GridSize[3])
+        Kz  = permutedims(repeat((kz),1,GridSize[1],GridSize[2]),[2 3 1]); # Meshgrid K_sq
+        println("    Creating 3D Cartesian grid with ", Nx, " x ", Ny, " x ", Nz, " points.")
+        return x, y, z, X, Y, Z, kx, ky, kz, Kx, Ky, Kz, facx, facy, facz
     end
 end 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -25,3 +25,45 @@ using Test
         )
     end
 end
+
+@testset "CartGrid" begin
+    # This begins a set of tests for the CartGrid function
+    @testset "1D grid setup" begin
+        x, kx, facx = QSpin.Grids.CartGrid([10.0], [4]);
+        @test isapprox(x, [-10.0, -5.0, 0.0, 5.0])
+        @test isapprox(kx, [0, 1, -2., -1.].*(pi/10))
+        @test isapprox(facx, pi / 10)
+    end
+    @testset "2D grid setup" begin
+        x, y, X, Y, kx, ky, Kx, Ky, facx, facy = QSpin.Grids.CartGrid([10.0, 5.0], [4,10]);
+        @test isapprox(x, [-10.0, -5.0, 0.0, 5.0])
+        @test isapprox(y, range(-5.0, stop=4, length=10))
+        @test isapprox(X[1,:],X[2,:]) # Check that X is constant along the y direction
+        @test isapprox(Y[:,1],Y[:,2]) # Check that Y is constant
+        @test isapprox(kx, [0, 1, -2., -1.].*(pi/10))
+        @test isapprox(ky, [range(0,stop=4,length=5) ; range(-5,stop=-1,length=5)].*(pi/5))
+        @test isapprox(Kx[1,:],Kx[2,:]) # Check that Kx is constant along the y direction
+        @test isapprox(Ky[:,1],Ky[:,2]) # Check that Ky is constant along the x direction
+        @test isapprox(Ky[:,1],ky) # Check that Ky is consistent with ky
+        @test isapprox(Kx[1,:],kx) # Check that Kx is consistent with kx
+        @test isapprox(facx, kx[2])
+        @test isapprox(facy, ky[2])
+    end
+    @testset "3D grid setup" begin
+        x, y, z, X, Y, Z, kx, ky, kz, Kx, Ky, Kz, facx, facy, facz = QSpin.Grids.CartGrid([10.0, 5.0, 20.], [4,10, 20]);
+        @test isapprox(x, [-10.0, -5.0, 0.0, 5.0])
+        @test isapprox(y, range(-5.0, stop=4, length=10))
+        @test isapprox(z, range(-20.0, stop=18, length=20))
+        @test isapprox(kx, [0, 1, -2., -1.].*(pi/10))
+        @test isapprox(ky, [range(0,stop=4,length=5) ; range(-5,stop=-1,length=5)].*(pi/5))
+        @test isapprox(kz, [range(0,stop=9,length=10) ; range(-10,stop=-1,length=10)].*(pi/20))
+         @test isapprox(Kx[1,:,1],Kx[2,:,5]) # Check that Kx is constant along the y direction
+        @test isapprox(Ky[:,1,1],Ky[:,2,3]) # Check that Ky is constant along the x direction
+        @test isapprox(Ky[:,1,1],ky) # Check that Ky is consistent with ky
+        @test isapprox(Kx[1,:,1],kx) # Check that Kx is consistent with kx
+        @test isapprox(facx, kx[2])
+        @test isapprox(facy, ky[2])
+        @test isapprox(facz, kz[2])
+    end
+end
+ 
