26a59ebd4fcdec0fa1d8ddfbcb270ef1a06c15e3
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-24 15:39:12 +0000

    Superflous changes

--- scripts/gpe_tp.jl
+++ scripts/gpe_tp.jl
@@ -2,8 +2,9 @@ using QSpin
 using FFTW
 using MAT, Random
 using Plots, LaTeXStrings
-FFTW.set_num_threads(4)
-# Parameter setup for the GPE simulation
+
+Random.seed!(42)
+FFTW.set_num_threads(6)
 
 ParaIn = Dict(
     "g" => 1.0,
@@ -25,31 +26,9 @@ x, y, X, Y, kx, ky, Kx, Ky, facx, facy =
 trap = 0.5 * (X .^ 2 + Y .^ 2)
 KE_mtx = 0.5 * (Kx .^ 2 + Ky .^ 2)
 
-# Setting the equation of motion for the target problem
-"""
-    KE(ψ::Array{ComplexF64}, time::Float64)
-
-    Creating the function of kinetic energy term in the Hamiltonian.
-
-"""
 KE = QSpin.Grids.fft_ke(KE_mtx)
-
-"""
-    Pot(trap::Array{Float64}, ψ::Array{ComplexF64})
-
-    Creating the fucntion of the potential term in the Hamiltonian.
-
-    Here we take the GPE in the rotating frame as an example, so the potential term includes the external trap potential, the nonlinear interaction term, and the rotation term.
-
-    :param trap: the external trap potential, which is a two-dimensional array in this example.
-    :param ψ: the field variable, which is a two-dimensional complex array in this example
-"""
-#function Pot(ψ::Array{ComplexF64},time)
-#    pot = (trap.-μ) .* ψ + g .* (abs.(ψ).^2).* ψ - Ω * QSpin.Grids.fft_Lzψ(X,Y,Kx,Ky)(ψ,time)
-#    return pot
-#end
-
 Lz = QSpin.Grids.fft_Lzψ(X, Y, Kx, Ky)
+
 function Pot(ψ::Array{ComplexF64}, time)
     pot = (trap .- ParaIn["μ"]) .* ψ + ParaIn["g"] .* (abs.(ψ) .^ 2) .* ψ
     ang_mom = ParaIn["Ω"] * Lz(ψ, time)
@@ -58,10 +37,9 @@ end
 
 Hamil = QSpin.Hamiltonian.hamiltonian(KE, Pot, ParaIn["irt"])
 
-# Initial condition
 ψ0::Array{ComplexF64} =
-    sqrt(50.0) .* exp.(-((X) .^ 2+Y .^ 2)/2) .* X .* Y +
-    0.01*randn(ComplexF64, (length(x), length(y)))
+    sqrt(50.0) .* exp.(-((X) .^ 2 + Y .^ 2) / 2) .* X .* Y +
+    0.01 * randn(ComplexF64, (length(x), length(y)))
 
 # Solving the GPE
 println("Simulation begins")
@@ -79,7 +57,7 @@ anim = @animate for i = 1:length(t)
             L"\mathrm{Rotating BEC}, \Omega=",
             ParaIn["Ω"],
             L" \omega, i\omega\tau=",
-            round(t[i]*10)/10,
+            round(t[i] * 10) / 10,
         ),
         xlims = (-ParaIn["Xmax"], ParaIn["Xmax"]),
         ylims = (-ParaIn["Ymax"], ParaIn["Ymax"]),
@@ -88,7 +66,4 @@ anim = @animate for i = 1:length(t)
     )
 end
 
-# Save as GIF
-gif(anim, string("outputs/gpe_imt-Om=", ParaIn["Ω"], ".gif"), fps = 50)
-# To save as mp4 instead:
-# gif(anim, "sine_wave.mp4", fps=30)
+gif(anim, string("outputs/gpe_imt-Om=", ParaIn["Ω"], "-DE.gif"), fps = 50)
