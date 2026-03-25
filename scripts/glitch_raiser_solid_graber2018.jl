eab835d09a94e86e4ed34848afa14730e846aac1
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-25 15:04:30 +0000

    Tidy up glitch raiser script

--- scripts/glitch_raiser_solid_graber2018.jl
+++ scripts/glitch_raiser_solid_graber2018.jl
@@ -18,43 +18,21 @@ Setting the equation of motion for the target problem
     :param ψ: variable/vector/array associated with the problem.
     "param time: the time of the problem
 """
-function eom_desolve(dψ::AbstractArray, ψ::AbstractArray, parameters, time::Float64)
+function eom(dψ::AbstractArray, ψ::AbstractArray, parameters, time::Float64)
     dψ .= M * ψ
 end
 
-de_problem = DE.ODEProblem(eom_desolve, ψ0, t_span)
+problem = DE.ODEProblem(eom, ψ0, t_span)
+u = DE.solve(problem, DE.Tsit5(), dt = dt, saveat = Dt)
 
-@time u_desolve = DE.solve(de_problem, DE.Tsit5(), dt = dt, saveat = Dt)
-t_desolve = u_desolve.t
-
-desolve_plot = plot(t_desolve, u_desolve[1, :])
+output_plot = plot(u_desolve.t, u_desolve[1, :])
 plot!(
-    desolve_plot,
-    t_desolve,
-    u_desolve[2, :],
+    output_plot,
+    u.t,
+    u[2, :],
     xlabel = "time (A.U.)",
     ylabel = "Rotating Frequency (A.U.)",
     title = "Solving a set of coupled ODEs [DESolve]",
 )
 
-function eom(ψ::AbstractArray, time::Float64)
-    return M * ψ
-end
-
-@time u_qspin, t_qspin = QSpin.OdeSolve.evolve_rk4([0.1; 0.2], 1e-3, 1e-1, 1.0, eom)
-
-qspin_plot = plot(t_qspin, u_qspin[1, :])
-plot!(
-    qspin_plot,
-    t_qspin,
-    u_qspin[2, :],
-    xlabel = "time (A.U.)",
-    ylabel = "Rotating Frequency (A.U.)",
-    title = "Solving a set of coupled ODEs [QSpin]",
-)
-
-savefig(desolve_plot, "./outputs/desolve.png")
-savefig(qspin_plot, "./outputs/qspin.png")
-
-println("Timestep difference:", maximum(abs2, (t_qspin - t_desolve)))
-println("Solution difference:", maximum(abs2, (u_qspin - u_desolve)))
+savefig(output_plot, "./outputs/glitch-raiser-output.png")
