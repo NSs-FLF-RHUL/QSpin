124c1fdf5a0e2ffe40b90aecbe570975ed068537
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-25 13:50:19 +0000

    Add in-place hamiltonian function

--- src/Hamiltonian.jl
+++ src/Hamiltonian.jl
@@ -27,3 +27,42 @@ function hamiltonian(KineticEnergy, PotentialEnergy, irt)
     return internal_hamiltonian
 end
 end
+
+"""
+    hamiltonian!(KineticEnergy, PotentialEnergy, irt = im)
+
+Return a function H!(dψ, ψ, parameters, time) that evaluates the Hamiltonian formed from the
+given Kinetic and Potential energy functions, at the given time.
+
+The ``parameters`` argument is passed to the ``KineticEnergy`` and ``PotentialEnergy`` functions when evaluating
+``H!``.
+
+:param KineticEnergy: a function that computes the kinetic energy term in the equation of motion, which takes ψ and time as input.
+:param PotentialEnergy: a function that computes the potential energy term in the equation of motion, which takes ψ and time as input.
+:param irt: the propagation factor, which can be -1 for imaginary time evolution, -im for real time evolution, or a general complex value for a dissipative evolution, namely, dGPE.
+"""
+function hamiltonian!(KineticEnergy, PotentialEnergy, irt = im)
+    """
+        internal_hamiltonian!(dψ::AbstractArray, ψ::AbstractArray, parameters, time::Float64)
+
+    Evaluate the Hamiltonian at the given time and field ψ in-place, overwriting the input array dψ.
+
+    :param dψ: Array to update in-place.
+    :param ψ: Current field values.
+    :param parameters: Container holding additional parameters needed by the equations of motion.
+    :param time: Current time.
+    """
+    function internal_hamiltonian!(
+        dψ::AbstractArray,
+        ψ::AbstractArray,
+        parameters,
+        time::Float64,
+    )
+        dψ .=
+            irt .* (
+                KineticEnergy(ψ, time; parameters...) +
+                PotentialEnergy(ψ, time; parameters...)
+            )
+    end
+    return internal!
+end
