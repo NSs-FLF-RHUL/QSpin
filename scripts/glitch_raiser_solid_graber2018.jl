ef19c8e8d895c03382796ed5760c159ffe1b19e8
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-25 13:08:38 +0000

    Do a comparison in glitch script

--- scripts/glitch_raiser_solid_graber2018.jl
+++ scripts/glitch_raiser_solid_graber2018.jl
@@ -2,18 +2,15 @@
 using Plots
 using QSpin
 
-# Parameter Setup according to V. Graber et al., ApJ 865, 23 (2018)
-Msun = 1.989e30 # solar mass in kg
-R = 10.e3# NS radius in m
-BSF = 1e-2 #[1e-3,1e-2]
-Bcore = 1e-2 # [3e-3,1e-2]
-Itot = 0.35 * Msun * R^2
-ISF = 0.95 * Itot
-Icore = 0.05 * Itot
-Icrust = 0.01599339442028418 * Itot
-Next = 1.2e2
+import OrdinaryDiffEq as DE
 
-Ω0 = [70.34 - 6.3e-3; 70.34; 70.34]
+M = [-2 1; 1 -2]
+ψ0 = [0.1; 0.2]
+dt = 1e-3
+Dt = 1e-1
+t_start = 0.0
+t_end = 1.0
+t_span = (t_start, t_end)
 
 """
 Setting the equation of motion for the target problem
@@ -21,21 +18,43 @@ Setting the equation of motion for the target problem
     :param ψ: variable/vector/array associated with the problem.
     "param time: the time of the problem
 """
-function eom(ψ::Array{Float64}, time::Float64)
-    Mtx = [-2 1; 1 -2]
-    return Mtx * ψ
+function eom_desolve(dψ::AbstractArray, ψ::AbstractArray, parameters, time::Float64)
+    dψ .= M * ψ
 end
 
-# Running and Plotting
-@time ut, t = QSpin.OdeSolve.evolve_rk4([0.1; 0.2], 1e-3, 1e-1, 1.0, eom)
-output_plot = plot(t, ut[1, :])
+de_problem = DE.ODEProblem(eom_desolve, ψ0, t_span)
+
+@time u_desolve = DE.solve(de_problem, DE.Tsit5(), dt = dt, saveat = Dt)
+t_desolve = u_desolve.t
+
+desolve_plot = plot(t_desolve, u_desolve[1, :])
 plot!(
-    output_plot,
-    t,
-    ut[2, :],
+    desolve_plot,
+    t_desolve,
+    u_desolve[2, :],
     xlabel = "time (A.U.)",
     ylabel = "Rotating Frequency (A.U.)",
-    title = "Solving a set of coupled ODEs",
+    title = "Solving a set of coupled ODEs [DESolve]",
 )
 
-savefig(output_plot, "./outputs/output-plot.png")
+function eom(ψ::AbstractArray, time::Float64)
+    return M * ψ
+end
+
+@time u_qspin, t_qspin = QSpin.OdeSolve.evolve_rk4([0.1; 0.2], 1e-3, 1e-1, 1.0, eom)
+
+qspin_plot = plot(t_qspin, u_qspin[1, :])
+plot!(
+    qspin_plot,
+    t_qspin,
+    u_qspin[2, :],
+    xlabel = "time (A.U.)",
+    ylabel = "Rotating Frequency (A.U.)",
+    title = "Solving a set of coupled ODEs [QSpin]",
+)
+
+savefig(desolve_plot, "./outputs/desolve.png")
+savefig(qspin_plot, "./outputs/qspin.png")
+
+println("Timestep difference:", maximum(abs2, (t_qspin - t_desolve)))
+println("Solution difference:", maximum(abs2, (u_qspin - u_desolve)))
