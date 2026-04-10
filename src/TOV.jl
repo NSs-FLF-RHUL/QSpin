950c40f1ce7ef73d188838353e34baa933427b77
William Graham <ccaegra@ucl.ac.uk>
2026-04-10 15:56:02 +0100

    Move the EoS definitions for the polytrope into the TOV module

--- src/TOV.jl
+++ src/TOV.jl
@@ -24,20 +24,21 @@ To solve the TOV equation we must also provide an equation of state (EoS) that r
 Note that we can solve the TOV equation(s) numerically using the vector ``u = (P, m)`` and solving a first-order system in ``u``.
 """
 
-using ..PhysicalConstants: gravitational_constant, speed_of_light_vacuum
+using ..Parameters: ParameterType
+using ..PhysicalConstants: hbar, gravitational_constant, neutron_mass, speed_of_light_vacuum
 
 """
 Return a function that evaluates the TOV equation.
 
 Returns a function `tov!(du, u, params, r)` that evaluates the TOV equation (in vector form) given the EoS relationship ``\\rho(P)``.
 
-`Eos_inv` is assumed to take `(P, parameters, r)` as inputs, where `P = u[1]` is the pressure.
+`EoS_rho_to_P` is assumed to take `(P, parameters, r)` as inputs, where `P = u[1]` is the pressure.
 """
-function tov_eq!(EoS_inv::Function)
+function tov_eq!(EoS_rho_to_P::Function)
     function tov_inner!(du, u, params, r)
         P = u[1]
         m = u[2]
-        rho = EoS_inv(P);
+        rho = EoS_rho_to_P(P);
         if r == 0.0
             du[1] .= 0.0;
         else
@@ -50,4 +51,83 @@ function tov_eq!(EoS_inv::Function)
         du[2] .= 4*π*r^2*rho;
     end
     return tov_inner!
+end
+
+"""
+Create EoS functions for two-component polytrope neutron stars.
+
+Such stars are assumed to be spheres composed of non-interacting, degenerate neutrons that are characterised by two EoS, in the core and crust regions.
+The boundary between the regions is located at some radial coordinate ``r = \\rho_b``;
+
+``
+P =
+\\begin{cases}
+K_{crust} \\rho^{\\gamma_{crust}} & \\rho < \\rho_b, \\
+K_{core} \\rho^{\\gamma_{core}} & \\rho > \\rho_b,
+\\end{cases}
+&\\qquad
+K_{core} = K_{crust} \\rho_b^{\\gamma_{crust}-\\gamma_{core}},
+``
+
+with the equation for ``K_{core}`` being a result of imposing pressure continuity at the crust-core interface.
+
+We further assume that the crust is pure degenerate neutron matter;
+
+``
+P_{crust} = \\frac{(3\\pi^2)^{2/3}}{5} \\frac{\\bar{h}^2}{m_n^{8/3}} \\rho^{5/3}.
+``
+
+`parameters` is assumed to define the following quantities:
+
+- ``K_{crust}``, under the name ``K_crust``.
+- ``\\gamma_{crust}``, under the name `gamma_crust`.
+- ``\\gamma_{core}``, under the name `gamma_core`.
+- ``\rho_b``, under the name `rho_b`.
+
+# Arguments
+- `parameters::ParameterType`: Parameter values for the two-component polytrope model.
+- `report_transition_pressure::Bool`: If `true`, the transition pressure at the interface will be printed.
+
+# Returns
+- `EoS_P_to_rho::Function`: ``P(\\rho)`` as a function called with `(rho, params, r)`.
+- `EoS_rho_to_P::Function`: ``\\rho(P)`` as a function called with `(P, params, r)`.
+"""
+function EoS_two_component_polytrope(
+    parameters::ParameterType,
+    report_transition_pressure::Bool = false,
+)
+    K_core =
+        parameters.K_crust *
+        parameters.rho_b ^ (parameters.gamma_crust - parameters.gamma_core);
+
+    function EoS_P_to_rho(rho, params, r)
+        if rho < 0
+            P = 0.0;
+        elseif rho <= parameters.rho_b
+            P = parameters.K_crust * rho .^ parameters.gamma_crust;
+        else
+            P = K_core * rho .^ parameters.gamma_core;
+        end
+        return P;
+    end
+
+    # Pressure at the crust-core transition for continuity check
+    P_b = EoS_P_to_rho(parameters.rho_b, (), 0.0);
+
+    if report_transition_pressure
+        println("Pressure at crust-core transition (Pb): ", P_b)
+    end
+
+    function EoS_rho_to_P(P, params, r)
+        if P < 0
+            rho = 0.0;
+        elseif P <= P_b
+            rho = (P / parameters.K_crust) .^ (1/parameters.gamma_crust);
+        else
+            rho = (P / K_core) .^ (1/parameters.gamma_core);
+        end
+        return rho
+    end
+
+    return EoS_P_to_rho, EoS_rho_to_P
 end
