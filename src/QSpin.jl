da0a931fbbe5065616f1a9db9e0b312f32ba4229
William Graham <ccaegra@ucl.ac.uk>
2026-04-10 13:59:01 +0100

    Expose module via QSpin package

--- src/QSpin.jl
+++ src/QSpin.jl
@@ -3,6 +3,9 @@ module QSpin
 # Load ParameterType for solvers
 include("Parameters.jl")
 
+# Load physical constants that have no dependencies
+include("PhysicalConstants.jl")
+
 include("OdeSolve.jl")
 include("Grids.jl")
 include("Hamiltonian.jl")
