8572a4d52170f74a30907350521461c065bbdd7b
Gary Liu <liueruey@hotmail.com>
2026-05-21 14:35:40 +0100

    load PhysicalConstants

--- src/QSpin.jl
+++ src/QSpin.jl
@@ -2,9 +2,9 @@ module QSpin
 
 # Load ParameterType for solvers
 include("Parameters.jl")
-
 include("OdeSolve.jl")
 include("Grids.jl")
 include("Hamiltonian.jl")
-
+include("PhysicalConstants.jl")
+include("MFriction.jl")
 end
