c1bd2f0ffb8e90a5bcf863ef117afc381afd524c
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-11 14:16:15 +0100

    Clarify example use-cases

--- src/TOV/EoS/EquationOfState.jl
+++ src/TOV/EoS/EquationOfState.jl
@@ -17,7 +17,20 @@ Non-dimensionalise functions returned from an equation of state, according to th
 
 Characteristic lengths should be provided in a `NamedTuple` of the same format as the output of `QSpin.TOV.characteristic_lengths_TOV`.
 
-`builder` functions (like those provided in `QSpin.TOV.EquationOfState`) return two functions; one that evaluates pressure ``P`` as a function of density ``\\rho`` and another that does the inverse (in that order). This function mutates these outputs to instead by functions of the corresponding dimensionless pressure and dimensionless density.
+`builder` functions (like those provided in `QSpin.TOV.EquationOfState`) return two functions; one that evaluates pressure ``P`` as a function of density ``\\rho`` and another that does the inverse (in that order). This function mutates these outputs to instead by functions of the corresponding dimensionless pressure and dimensionless density. For example, if you were previously doing
+
+```julia
+EoS_P_from_rho, EoS_rho_from_P = builder(args...; kwargs...)
+```
+
+and want to turn `EoS_P_from_rho` and `EoS_rho_from_P` into their non-dimensional equivalents, using characteristic lengths with ``R = 1`` say, you would run
+
+```julia
+using QSpin.TOV: characteristic_lengths_TOV
+
+char_lengths = characteristic_lengths_TOV(; length = 1.0)
+nd_EoS_P_from_rho, nd_EoS_rho_from_P = nondimensional_EoS(char_lengths, builder, args...; kwargs...)
+```
 
 # Arguments
 - `characteristic_lengths::NamedTuple`: Mapping of characteristic length names to values.
