0f2188b7c6535eeaa372d883ab56bc8eb0ef1f7b
Gary Liu <liueruey@hotmail.com>
2026-07-17 09:56:45 +0100

    Add EoS_LInterp

---
+++ src/TOV/EoS/EoS_LInterp.jl
@@ -0,0 +1,20 @@
+using DocStringExtensions: TYPEDSIGNATURES
+using ....Parameters: ParameterType
+using Roots: find_zero
+using CSV
+using DataFrames
+using DataInterpolations: ExtrapolationType, QuadraticSpline
+
+function EoS_Linterp(file_path, EoS_indices)
+    df = DataFrame(CSV.File(file_name, delim = " "))
+    df = select!(df, [k for (k, v) in pairs(eachcol(df)) if !all(ismissing, v)])
+    #deleteat!(permutedims(df), all.(ismissing, eachcol(df))) |> permutedims # Seems to be equivalent.
+    i_rho, i_press = EoS_indices
+    rho = df[:, i_rho]
+    press = df[:, i_press]
+    EoS_P_from_rho =
+        QuadraticSpline(press, rho; extrapolation = ExtrapolationType.Extension);
+    EoS_rho_from_P =
+        QuadraticSpline(press, rho; extrapolation = ExtrapolationType.Extension);
+    return EoS_P_from_rho, EoS_rho_from_P
+end
