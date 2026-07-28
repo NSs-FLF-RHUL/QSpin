2f34f59080127a7ce59aee5c5f03e07273836655
Gary Liu <liueruey@hotmail.com>
2026-07-28 13:47:15 +0100

    upate example script for different initial condition

--- scripts/TOV_scripts/tov_eq_v1.jl
+++ scripts/TOV_scripts/tov_eq_v1.jl
@@ -12,18 +12,19 @@ using QSpin.Parameters: ParameterType
 using QSpin.TOV: TOV_Solve
 using QSpin.TOV.EquationOfState: EoS_GCA2018
 using Plots, LaTeXStrings
-
+using QSpin.PhysicalConstants: neutron_mass, mass_sun
 Sim_Input = (
-    ρ0 = 5e15, # Initial central density in g/cm^3, above 2e16 seems to be unstable
+    ρ0 = 0.08*1e39*neutron_mass*1e3, # Initial central density in g/cm^3, above 2e16 seems to be unstable
     dr = 0.0001*1e5, # Radial step in cm
     Dr = 0.005*1e5, # Radial interval for recording values in cm
-    r_beg = 0.e5,
+    r_beg = 10*1e5,
+    M_core = 1.4 * mass_sun * 1e3,
     units = "CGS", # optional
 );
 
 # Calling EoS_GCA2018
 EoS, EoS_inv = EoS_GCA2018();
-u0 = [EoS(Sim_Input.ρ0); 0.0];
+u0 = [EoS(Sim_Input.ρ0); Sim_Input.M_core];
 
 # Callback setup to terminate the integration when the pressure drops below zero
 @time TOV_sol = TOV_Solve(
@@ -33,9 +34,8 @@ u0 = [EoS(Sim_Input.ρ0); 0.0];
     Sim_Input.Dr,
     Sim_Input.r_beg;
     reltol = 1e-13,
-    abstol = 1e-13,
     input_units = Sim_Input.units, # optional
     rho_ref = 2.8e14, # optional - nuclear saturation density in g/cm^3
 )
 
-plot!(TOV_sol.r*1e-5, TOV_sol.ρr)
+plot(TOV_sol.r*1e-5, TOV_sol.ρr)
